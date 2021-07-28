9a3853554861295c2fe5be3c8a4d86215075a6a8
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-07-27 22:25:39 -0500

    Basic design part 2

--- lib/screens/basic-design.dart
+++ lib/screens/basic-design.dart
@@ -5,8 +5,34 @@ class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/landscape.jpg'))
+      body: Column(
+        children: [
+          Image(image: AssetImage('assets/landscape.jpg')),
+          _Title()
+        ],
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Oeschinen Lake Campground', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('Kandersteg, Switzerland', style:  TextStyle(color: Colors.black45)),
+            ],
+          ),
+          Expanded(child: Container()),
+          Icon(Icons.star,color: Colors.red),
+          Text('45')
+        ],
       ),
     );
   }
