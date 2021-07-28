bb83be1ac1f46b7ade5fd73f92d7ce7a2cc14c5e
Jorge Luis Alonso <alonso.jorgeluis.01@gmail.com>
2021-07-28 09:15:54 -0500

    Basic design part 3

--- lib/screens/basic-design.dart
+++ lib/screens/basic-design.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
 
 class BasicDesignScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Image(image: AssetImage('assets/landscape.jpg')),
-          _Title()
+          _Title(),
+          _ButtonSection(),
+          _TextSection()
         ],
       ),
+    );
+  }
+}
+
+class _TextSection extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal:30.0),
+      child: Text('But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who'),
+    );
+  }
+}
+
+class _ButtonSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          CustomButton(icon: Icons.call,text: 'Call'),
+          CustomButton(icon: Icons.router,text: 'Route'),
+          CustomButton(icon: Icons.share,text: 'Share'),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButton({Key? key, required this.icon, required this.text}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(this.icon, color: Colors.blue, size: 30.0),
+        Text(this.text, style: TextStyle(color: Colors.blue))
+      ],
     );
   }
 }
@@ -25,12 +70,14 @@ class _Title extends StatelessWidget {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Oeschinen Lake Campground', style: TextStyle(fontWeight: FontWeight.bold)),
-              Text('Kandersteg, Switzerland', style:  TextStyle(color: Colors.black45)),
+              Text('Oeschinen Lake Campground',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('Kandersteg, Switzerland',
+                  style: TextStyle(color: Colors.black45)),
             ],
           ),
           Expanded(child: Container()),
-          Icon(Icons.star,color: Colors.red),
+          Icon(Icons.star, color: Colors.red),
           Text('45')
         ],
       ),
